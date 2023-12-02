f9e20849d2a158fc6ce4a2c9a64a374591b827ee
Gunzino <gunnarguy@me.com>
2023-12-02 00:13:44 -0800

    Add hidden class to hide elements and update dropdown.js

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,16 +1,5 @@
 // JavaScript for Dropdown functionality
 document.getElementById('headerDropdownButton1').addEventListener('click', function() {
     var content = document.getElementById('headerDropdownContent1');
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-    } else {
-        content.classList.add('hidden');
-    }
+    content.classList.toggle('show');
 });
-    var content = document.getElementById('headerDropdownContent2');
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-    } else {
-        content.classList.add('hidden');
-    }
-}
