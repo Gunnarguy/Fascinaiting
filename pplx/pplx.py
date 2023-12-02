4d575a4075fc19554da2acd3e84b2fa102bfd4e4
Gunzino <gunnarguy@me.com>
2023-12-01 18:40:00 -0800

    pplx-tune

---
+++ pplx/pplx.py
@@ -0,0 +1,41 @@
+import requests
+
+def chat_with_pplx(user_input, previous_messages):
+    url = "https://api.perplexity.ai/chat/completions"
+    payload = {
+        "model": "pplx-7b-online",
+        "messages": previous_messages + [
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": "Bearer <YOUR_AUTHORIZATION_TOKEN>"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()
+
+def main():
+    previous_messages = [
+        {
+            "role": "system",
+            "content": "Be precise and concise."
+        }
+    ]
+    print("Starting conversation with PPLX model. Type 'quit' to exit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == 'quit':
+            break
+        response = chat_with_pplx(user_input, previous_messages)
+        model_response = response['choices'][0]['message']['content']
+        print("PPLX:", model_response)
+        previous_messages.append({"role": "user", "content": user_input})
+        previous_messages.append({"role": "assistant", "content": model_response})
+
+if __name__ == "__main__":
+    main()
