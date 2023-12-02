3d39844f183e6e335ffb231d9a5c1b4989fa7ade
Gunzino <gunnarguy@me.com>
2023-12-01 23:36:39 -0800

    Add dropdown button styles and update package dependencies

---
+++ scripts/dropdown.js
@@ -0,0 +1,16 @@
+// JavaScript for Dropdown functionality
+document.getElementById('headerDropdownButton1').addEventListener('click', function() {
+    var content = document.getElementById('headerDropdownContent1');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+});
+    var content = document.getElementById('headerDropdownContent2');
+    if (content.classList.contains('hidden')) {
+        content.classList.remove('hidden');
+    } else {
+        content.classList.add('hidden');
+    }
+}
