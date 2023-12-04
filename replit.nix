b874844aa0cacbba6eabb31e85fadcc35af88563
Gunnarguy <gunnarguy@me.com>
2023-12-04 06:54:42 +0000

    More Replit stuff

--- replit.nix
+++ replit.nix
@@ -1,7 +1,15 @@
 { pkgs }: {
   deps = [
+    pkgs.glibcLocales
+    pkgs.glibc
+    pkgs.nano
     pkgs.zsh
     pkgs.nodePackages.vscode-langservers-extracted
     pkgs.nodePackages.typescript-language-server  
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.glibcLocales
+    ];
+  };
 }
