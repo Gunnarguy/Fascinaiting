2f03cccda7476ef0a76577f87f2199de70b81fd8
Gunzino <gunnarguy@me.com>
2023-12-04 22:08:29 -0800

    Fix dropdown menu functionality

---
+++ Templates/js/script.js
@@ -0,0 +1,16 @@
+
+// JavaScript for responsive menu and other interactive elements
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Code for responsive menu toggle
+    // This is a placeholder for the actual implementation
+    const menuToggle = document.querySelector('#menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+
+    // Additional interactive elements can be added here
+});
+
