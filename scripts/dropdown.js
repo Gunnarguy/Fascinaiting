e5bbf4f45d65784e00a54e8e732774f3e37f46eb
Gunzino <gunnarguy@me.com>
2023-12-06 16:35:43 -0800

    Remove dropdown functionality

--- scripts/dropdown.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for Dropdown functionality
-document.getElementById('headerDropdownButton1').addEventListener('click', function() {
-    var content = document.getElementById('headerDropdownContent1');
-    content.classList.toggle('show');
-});
