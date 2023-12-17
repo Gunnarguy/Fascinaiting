14f8dae736979169781a125a34fc779960fa38e3
Gunzino <gunnarguy@me.com>
2023-12-17 00:18:59 -0800

    Fix formatting and add quotes to attribute values

--- src/App.js
+++ src/App.js
@@ -29,19 +29,19 @@ const SubscriptionForm = () => (
   <section style={{ textAlign: 'center', padding: '20px' }}>
     <h2>Subscribe to Our Newsletter</h2>
     <form>
-      <input type=email placeholder='Enter your email' style={{ marginRight: '10px' }}/>
-      <button type=submit>Subscribe</button>
+      <input type="email" placeholder='Enter your email' style={{ marginRight: '10px' }}/>
+      <button type="submit">Subscribe</button>
     </form>
   </section>
 );
 
-const App.js = () => (
+const App = () => (
   <div style={{ width: '70%', margin: '0 auto' }}>
     <NavigationBar />
     <h1>Welcome to the Blog!</h1>
     {/* Blog posts list */}
-    <BlogPostPreview title=Blog Post 1 excerpt=This is an excerpt from the first blog post. />
-    <BlogPostPreview title=Blog Post 2 excerpt=This is an excerpt from the second blog post. />
+    <BlogPostPreview title="Blog Post 1" excerpt="This is an excerpt from the first blog post." />
+    <BlogPostPreview title="Blog Post 2" excerpt="This is an excerpt from the second blog post." />
     {/* Advertisement */}
     <AdPlaceholder />
     {/* Subscription form */}
