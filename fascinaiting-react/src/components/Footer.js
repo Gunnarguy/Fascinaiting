58815e37ec0a4e1504ffcff8bc937cc73bef7910
Gunzino <gunnarguy@me.com>
2023-12-18 16:53:53 -0800

    Delete unused files and dependencies

--- fascinaiting-react/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer>
-    <p>&copy; {new Date().getFullYear()} FascinAIting. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function App() {
-    // Your component code here
-}
-
-export default App;
