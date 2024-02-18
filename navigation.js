e948ee1290d43a5cc88eade3e5b06e6bf7c81b65
Gunnarguy <gunnarguy@me.com>
2024-02-18 06:52:35 +0000

    aligned

---
+++ navigation.js
@@ -0,0 +1,7 @@
+// JavaScript function to toggle the menu
+function toggleMenu() {
+  const menu = document.querySelector('.nav-menu');
+  menu.classList.toggle('responsive');
+}
+// Remember to make sure this file is loaded after the HTML content is fully loaded
+// or attached to an event that listens for when the DOM is ready.
