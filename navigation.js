1967007368fc1d13ab7c6ad6656bdca9074eddcd
Gunnarguy <gunnarguy@me.com>
2024-02-20 06:03:12 +0000

    js canvas

--- navigation.js
+++ navigation.js
@@ -1,7 +1,4 @@
-// JavaScript function to toggle the menu
 function toggleMenu() {
-  const menu = document.querySelector('.nav-menu');
+  var menu = document.querySelector('.nav-menu');
   menu.classList.toggle('responsive');
 }
-// Remember to make sure this file is loaded after the HTML content is fully loaded
-// or attached to an event that listens for when the DOM is ready.
