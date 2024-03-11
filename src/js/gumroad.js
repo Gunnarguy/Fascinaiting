9503f6ff728b78e080dc1559af0c0d63f12f1a4c
gunnarguy <gunnarguy@me.com>
2024-03-10 21:16:13 -0700

    Update Gumroad embed code and styles

---
+++ src/js/gumroad.js
@@ -0,0 +1,5 @@
+<div>
+
+	<script src="https://gumroad.com/js/gumroad-embed.js"></script>
+	<div class="gumroad-product-embed"><script src="https://gumroad.com/js/gumroad.js"></script></div>
+</div>
