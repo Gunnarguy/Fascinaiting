47b0b204195f715193b4f65bb396e32787c1a58d
gunnarguy <gunnarguy@me.com>
2024-03-16 23:02:15 -0700

    Remove GUDID functionality and add Gumroad component

--- src/js/gumroad.js
+++ src/js/gumroad.js
@@ -1,5 +1,11 @@
-<div>
 
-	<script src="https://gumroad.com/js/gumroad-embed.js"></script>
-	<div class="gumroad-product-embed"><script src="https://gumroad.com/js/gumroad.js"></script></div>
-</div>
+import React from 'react';
+
+const MyComponent = () => (
+	<div>
+		<script src="https://gumroad.com/js/gumroad-embed.js"></script>
+		<div className="gumroad-product-embed"><script src="https://gumroad.com/js/gumroad.js"></script></div>
+	</div>
+);
+
+export default MyComponent;
